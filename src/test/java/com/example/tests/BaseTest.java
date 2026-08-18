263b92352dad5f0e1c04edbc94c6fcdd4593566b
Sheal354 <Sheal2004@gmail.com>
2026-08-18 20:44:13 +0400

    исправление работы с Selenoid

--- src/test/java/com/example/tests/BaseTest.java
+++ src/test/java/com/example/tests/BaseTest.java
@@ -11,6 +11,10 @@ public class BaseTest {
     @BeforeClass
     public void initBeforeClass() {
         Configuration.baseUrl = "https://qa.copy.mirapolis.ru/mira/";
+        String selenoidUrl = System.getProperty("selenoid.url");
+        if (selenoidUrl != null && !selenoidUrl.isEmpty()) {
+            Configuration.remote = selenoidUrl;
+        }
 //        Configuration.headless = true;
     }
 
